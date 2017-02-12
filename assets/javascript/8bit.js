9eed32ce6513ca0c6a4d3a818fbf94d18528ccb4
cl2112 <cl2112cl2112@gmail.com>
2017-02-11 23:35:14 -0500

    started reworking the design to be a static background

--- assets/javascript/8bit.js
+++ assets/javascript/8bit.js
@@ -3,23 +3,24 @@ $(document).ready(function(){
 generatePixels();
 
 
+
 function generatePixels(){
-	for (i=0; i<1000; i++){
-	$(".8bitProcessor").append("<div class='pixel "+ i +"' style='background-color: black; width: 2%; height: 2%; padding: 2%; float: left;opacity:0.2;'></div>")
+	for (i=0; i<1200; i++){
+	$(".bitProcessor").append("<div class='pixel "+ i +"' style='background-color: black; width: 2%; height: 2%; padding: 1%; float: left;opacity:0.2;'></div>")
 	}
 	pulsePixels();
 };
 
 function pulsePixels(){
 	setInterval(function(){
-		var randomPixel1 = "."+ Math.floor(Math.random()*1000);
-		var randomPixel2 = "."+ Math.floor(Math.random()*1000);
-		var randomPixel3 = "."+ Math.floor(Math.random()*1000);
-		var randomPixel4 = "."+ Math.floor(Math.random()*1000);
-		$(randomPixel1).fadeTo(2000, .9).delay(6000).fadeTo(1000, .2);
-		$(randomPixel2).fadeTo(2000, .9).delay(5000).fadeTo(1000, .2);
-		$(randomPixel3).fadeTo(2000, .9).delay(4000).fadeTo(1000, .2);
-		$(randomPixel4).fadeTo(2000, .9).delay(3000).fadeTo(1000, .2);
+		var randomPixel1 = "."+ Math.floor(Math.random()*1200);
+		var randomPixel2 = "."+ Math.floor(Math.random()*1200);
+		var randomPixel3 = "."+ Math.floor(Math.random()*1200);
+		var randomPixel4 = "."+ Math.floor(Math.random()*1200);
+		$(randomPixel1).fadeTo(2000, .9).delay(13000).fadeTo(5000, .2);
+		$(randomPixel2).fadeTo(2000, .9).delay(11000).fadeTo(5000, .2);
+		$(randomPixel3).fadeTo(2000, .9).delay(9000).fadeTo(5000, .2);
+		$(randomPixel4).fadeTo(2000, .9).delay(7000).fadeTo(5000, .2);
 	},100)
 }
 
